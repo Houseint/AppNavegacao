c2b9a576d4395af8ca649291ce41f56ce5423f9e
GustavoPitbull <83386216+Houseint@users.noreply.github.com>
2025-05-14 12:15:10 -0300

    Update LoginScreen.js

--- MeuAppDeNavegacao/src/screens/LoginScreen.js
+++ MeuAppDeNavegacao/src/screens/LoginScreen.js
@@ -7,17 +7,28 @@ const windowWidth = Dimensions.get('window').width;
 export default function LoginScreen ({navigation}){
         const [usuario, setUsuario] = useState('');
         const [senha, setSenha]= useState('');
-        const handleLogin = () => {
+        const Login = () => {
             if (!usuario || !senha) {
-                Alert.alert('Erro', 'Por favor, preencha todos os campos.');
+                alert('Erro. Preencha todos os campos.');
                 return;
             }
         
-            if (usuario === 'admin' && senha === '1234') {
-                navigation.navigate('Home');
-            } else {
-                Alert.alert('Erro', 'Usuário ou senha inválidos.');
+            if (usuario !== 'admin@gmail.com') {
+                alert('Erro. Usuário inválido.');
+                return;
             }
+        
+            if (senha !== '1234') {
+                alert('Erro. Senha incorreta.');
+                return;
+            }
+
+            if (senha !== '1234' && usuario !=='admin@gmail.com'){
+                alert("Usuário e senha incorretos");
+                return;
+            }
+        
+            navigation.navigate('Home');
         };
     return (
     <View style={styles.container}>
@@ -42,7 +53,7 @@ export default function LoginScreen ({navigation}){
             <View style={styles.buttonContainer}>
                 <Button
                     title="Login"
-                    onPress={() => navigation.navigate('Home')}
+                    onPress={Login}
                 />
                 </View>
           </View>
@@ -55,17 +66,29 @@ const styles= StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#A8CAE6', //cor de fundo da tela de login
+        backgroundColor: '#027373', //cor de fundo da tela de login
+        borderColor: '#000000',
+        borderRadius: 2
     },
     title: {
         fontSize: 24,
         marginBottom: 20,
         fontFamily: 'unageo',
+        color: '#A9D9D0'
     },
     buttonContainer: {
-        backgroundColor: '#add8e6',
+        backgroundColor: '#03A63C',
         margin: 10,
         width: windowWidth *0.5,
+        borderRadius: 7,
+        borderColor: '#000000',
+        color: '000000'
+    },
+    input: {
         borderRadius: 5,
+        borderColor: '#ffffff',
+        backgroundColor: '#F2E7DC',
+        borderWidth: 0.5,
+        margin: 3
     },
 });
